2704f0d8700e43ac9a815512e9f114fc06974064
pratik <pratikparmar477@gmail.com>
2026-08-03 20:36:54 +0530

    feat: add Django admin configuration for models

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,84 @@
 from django.contrib import admin
+from .models import (
+    Patient,
+    Provider,
+    Appointment,
+    AppointmentHistory,
+)
 
-# Register your models here.
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "email",
+    )
+
+    search_fields = (
+        "name",
+        "email",
+    )
+
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "specialty",
+    )
+
+    search_fields = (
+        "name",
+        "specialty",
+    )
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "patient",
+        "provider",
+        "appointment_date",
+        "appointment_time",
+        "appointment_type",
+        "status",
+        "version",
+    )
+
+    list_filter = (
+        "status",
+        "appointment_date",
+        "provider",
+    )
+
+    search_fields = (
+        "patient__name",
+        "provider__name",
+        "appointment_type",
+    )
+
+    ordering = (
+        "appointment_date",
+        "appointment_time",
+    )
+
+
+@admin.register(AppointmentHistory)
+class AppointmentHistoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "appointment",
+        "previous_status",
+        "new_status",
+        "changed_by",
+        "changed_at",
+    )
+
+    ordering = (
+        "-changed_at",
+    )
